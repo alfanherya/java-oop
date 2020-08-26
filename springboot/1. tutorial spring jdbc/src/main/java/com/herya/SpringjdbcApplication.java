3e376f44043c31dca5a1b1fbbb9685f6c9ba124c
alfanherya <alfanherya@gmail.com>
2020-08-26 08:04:14 +0700

    [alfan] update

--- springboot/1. tutorial spring jdbc/src/main/java/com/herya/SpringjdbcApplication.java
+++ springboot/1. tutorial spring jdbc/src/main/java/com/herya/SpringjdbcApplication.java
@@ -13,7 +13,7 @@ public class SpringjdbcApplication {
 		/*SpringApplication.run(SpringjdbcApplication.class, args);*/
 	String url_allowXtr = "jdbc:oracle:thin:@//cbtgrdnblnp-scn.intra.excelcom.co.id:1522/SOADBDEV";
 		Connection con = DriverManager.getConnection(url_allowXtr, "XLUATCONF03", "XLUATCONF03123");
-		System.out.println("connection estabilished lancar jaya ............");
+		System.out.println("connection estabilished lancar jaya semoga bisa ............");
 
 	}
 

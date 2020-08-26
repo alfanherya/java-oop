c92128546df96efdee0f56dcf500f1ce564bcac7
alfanherya <alfanherya@gmail.com>
2020-08-26 07:50:52 +0700

    [alfan] to check merge

---
+++ springboot/1. tutorial spring jdbc/src/main/java/com/herya/SpringjdbcApplication.java
@@ -0,0 +1,20 @@
+package com.herya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@SpringBootApplication
+public class SpringjdbcApplication {
+
+	public static void main(String[] args) throws Exception {
+		/*SpringApplication.run(SpringjdbcApplication.class, args);*/
+	String url_allowXtr = "jdbc:oracle:thin:@//cbtgrdnblnp-scn.intra.excelcom.co.id:1522/SOADBDEV";
+		Connection con = DriverManager.getConnection(url_allowXtr, "XLUATCONF03", "XLUATCONF03123");
+		System.out.println("connection estabilished lancar ............");
+
+	}
+
+}
